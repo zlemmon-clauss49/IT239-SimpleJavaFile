df2864b38c994bdf4f4f44f91e81706a4450e1c7
zlemmon-clauss49 <144113332+zlemmon-clauss49@users.noreply.github.com>
2026-01-05 09:50:14 -0500

    Update Hello.java
    "Fixed Syntax error"
    
    
    I hope, it'd be embarrassing if it was still broken.

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, world! From: Java :] OH NOES I'M ESPLODING! SUPER UN1337";
+        System.out.println("Hello, world! From: Java :] OH NOES I'M ESPLODING! SUPER UN1337)";
     }
 }
